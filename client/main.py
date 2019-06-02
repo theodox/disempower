1e93ae7a489c4b2147f9868ffcadb6d4d180429f
Steve Theodore <steve@theodox.com>
2019-06-02 12:45:23 -0700

    switch to urequests

--- client/main.py
+++ client/main.py
@@ -6,6 +6,16 @@ import time
 import network
 import socket
 import json
+import urequests as requests
+
+"""import logging
+logger = logging.getLogger('disempower')
+logger.setLevel(logging.DEBUG)
+
+logger.addHandler(logging.StreamHandler())
+fileh = logging.FileHandler('disempower.log')
+fileh.setLevel(logging.CRITICAL)
+"""
 
 
 def make_display():
@@ -29,8 +39,17 @@ def main_loop():
 
     LOGGED_IN = None
 
+    AVAIL, TOTAL = 0, 0
+
+    GREEH = Pin("LED_GREEN")
+    RED = Pin("LED_RED")
+
     while True and count < failsafe:
         count += 1
+
+        GREEH.value(AVAIL == 0)
+        RED.value(count % 2 == 0)
+
         try:
             result = READER.read_user_id(4)
         except RuntimeError:
@@ -45,13 +64,19 @@ def main_loop():
                 sock = socket.socket()
                 sock.connect(address)
 
-                req = "GET /check/{} HTTP/1.1\r\n\r\n".format(LOGGED_IN)
+                req = 'GET /check/{} HTTP/1.1\r\nHost = "theodox.pythonanywhere.com"\r\n'.format(LOGGED_IN)
 
                 sock.write(req.encode('utf-8'))
                 response = sock.read()
                 if response:
                     json_response = response.splitlines()[-1]
-                    print (json.loads(json_response))
+                    try:
+                        blob = json.loads(json_response)
+                    except:
+                        blob = {}
+                    AVAIL, TOTAL = blob.get(LOGGED_IN, (0, 0))
+
+                    print ("available", AVAIL, "total", TOTAL)
 
             time.sleep(0.2)
             continue
@@ -59,16 +84,25 @@ def main_loop():
         if LOGGED_IN == result:
             print (LOGGED_IN, "LOG OUT")
             LOGGED_IN = None
+            AVAIL = 0
+            TOTAL = 0
 
         else:
             print (LOGGED_IN, "LOG OUT")
             print (result, "LOG IN", len(result))
             LOGGED_IN = result
+            AVAIL = 0
+            TOTAL = 0
 
         time.sleep(4.0)
 
     print ("LOOP COMPLETE")
 
+
+REQ = '''GET /check/nicky HTTP/1.1
+Host: theodox.pythonanywhere.com
+Content-Type: text/html
+'''.encode('utf-8')
 
 if __name__ == '__main__':
 
@@ -76,9 +110,19 @@ if __name__ == '__main__':
     nic.active(True)
     nic.connect('simonides', '300spartans!')
 
+    r = requests.get("http://theodox.pythonanywhere.com/check/nicky")
+    print(r.json())
+    
+    nic.disconnect()
+
+"""
+
     time.sleep(4.0)
 
     try:
         main_loop()
+    except Exception as e:
+        logger.exception(e)
     finally:
         nic.disconnect()
+"""
