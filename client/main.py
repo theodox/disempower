9d417e087715e9e59ff2569d03ea498a3833cc98
theodox <steve@theodox.com>
2019-09-15 15:47:18 -0700

    first pass

--- client/main.py
+++ client/main.py
@@ -19,18 +19,19 @@ fileh.setLevel(logging.CRITICAL)
 
 POWER = pyb.Pin('X1', Pin.OUT_PP)
 
+
 def make_display():
     sda = Pin.board.Y10
     scl = Pin.board.Y9
     bus = I2C(sda=sda, scl=scl)
-    print ("Scan:", bus.scan())
+    print("Scan:", bus.scan())
     display = ssd1306.SSD1306_I2C(128, 32, bus)
 
     return display
 
 
 def main_loop():
-    print ("starting main loop")
+    print("starting main loop")
 
     DISPLAY = make_display()
     DISPLAY.poweron()
@@ -44,7 +45,7 @@ def main_loop():
     except AssertionError:
         DISPLAY.fill(0)
         DISPLAY.rect(1, 1, 126, 30, 1)
-        DISPLAY.text("no card reader", 8,12, 1)
+        DISPLAY.text("no card reader", 8, 12, 1)
         DISPLAY.show()
         return
 
@@ -71,6 +72,28 @@ def main_loop():
 
     BLINK = 1
 
+    def display_finished():
+        DISPLAY.fill(0)
+        DISPLAY.text("Out of time", 8, 12)
+        POWER.low()
+        GREEN.value(0)
+        BLUE.value(0)
+        RED.value(1)
+
+    def display_logged_out():
+        DISPLAY.text("Logged out", 8, 12)
+        POWER.low()
+
+    def display_logged_in():
+        DISPLAY.fill(0)
+        DISPLAY.text(str(LOGGED_IN), 8, 2)
+        DISPLAY.text(str(REMAIN), 8, 12)
+        DISPLAY.text(str(TOTAL), 8, 22)
+        avail = min(REMAIN / 10, 1.0)
+        h = int(32 * avail)
+        top = 32 - h
+        DISPLAY.fill_rect(100, top, 28, h, 1)
+
     while True:
 
         frame_time = time.ticks_ms()
@@ -87,18 +110,17 @@ def main_loop():
 #
         DISPLAY.fill(0)
         if LOGGED_IN:
-            DISPLAY.text(str(LOGGED_IN), 8, 2)
-            DISPLAY.text(str(REMAIN), 8, 12)
-            DISPLAY.text(str(TOTAL), 8, 22)
-
-            avail = min(REMAIN / 10, 1.0)
-            h = int(32 * avail)
-            top = 32 - h
-            DISPLAY.fill_rect(100, top, 28, h, 1)
-            POWER.high()
+            if REMAIN > 0.5:
+                display_logged_in()
+                POWER.high()
+            else:
+                display_finished()
+                LOGGED_IN = None
+                REMAIN = 0
+                TOTAL = 0
+                POWER.low()
         else:
-            DISPLAY.text("Logged out", 8, 12)
-            POWER.low()
+            display_logged_out()
 
         DISPLAY.show()
 
@@ -109,33 +131,31 @@ def main_loop():
                 result = READER.read_user_id(4)
                 NEXT_CARD = time.ticks_add(frame_time, CARD_TIME)
             except RuntimeError as e:
-                print ("CARD READ ERROR")
-                print (e)
+                print("CARD READ ERROR")
+                print(e)
                 result = None
                 DISPLAY.fill(0)
                 DISPLAY.text("Could not read card", 8, 12)
                 DISPLAY.show()
                 READER._wakeup()
-
-            except OSError as e:
-                print ("read timeout")
+            except OSError:
+                print("read timeout")
                 DISPLAY.fill(0)
                 DISPLAY.text("Card reader timeout", 8, 12)
                 DISPLAY.show()
                 READER._wakeup()
 
             if result:
-
                 if LOGGED_IN == result:
-                    print (LOGGED_IN, "LOG OUT")
+                    print(LOGGED_IN, "LOG OUT")
                     LOGGED_IN = None
                     REMAIN = 0
                     TOTAL = 0
 
                 else:
                     if LOGGED_IN:
-                        print (LOGGED_IN, "LOG OUT")
-                    print (result, "LOG IN", len(result))
+                        print(LOGGED_IN, "LOG OUT")
+                    print(result, "LOG IN", len(result))
                     LOGGED_IN = result
                     REMAIN = 0
                     TOTAL = 0
@@ -145,7 +165,7 @@ def main_loop():
 
             if LOGGED_IN is not None and len(LOGGED_IN):
 
-                print ("checking status")
+                print("checking status")
                 DISPLAY.fill(1)
                 DISPLAY.text("checking", 8, 12, 0)
                 DISPLAY.show()
@@ -168,7 +188,7 @@ def main_loop():
                 DISPLAY.fill(0)
                 DISPLAY.show()
 
-    print ("LOOP COMPLETE")
+    print("LOOP COMPLETE")
 
 
 if __name__ == '__main__':
